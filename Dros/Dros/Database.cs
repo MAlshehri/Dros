e6d81a838e4cecafbe543e7d12d6d01ca2b9114f
Mohammad M Alshehri <MAlshehri@DESKTOP-G3CCOBE>
2018-01-27 22:28:13 +0300

    secons

---
+++ Dros/Dros/Database.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Realms;
+
+namespace Dros
+{
+    public static class Database
+    {
+        private static Realm database;
+
+        public static Realm Instance
+        {
+            get
+            {
+                if(database == null)
+                {
+                    var assembly = typeof(Database).GetTypeInfo().Assembly;
+                    var bytes = GetEmbeddedResourceBytes(assembly, "dros.realm");
+
+#if __IOS__
+var resourcePrefix = "WorkingWithFiles.iOS.";
+#endif
+#if __ANDROID__
+                    var resourcePrefix = "WorkingWithFiles.Droid.";
+#endif
+
+                }
+                return database;
+            }
+        }
+
+        public static byte[] GetEmbeddedResourceBytes(Assembly assembly, string resourceFileName)
+        {
+            var stream = GetEmbeddedResourceStream(assembly, resourceFileName);
+
+            using (var memoryStream = new MemoryStream())
+            {
+                stream.CopyTo(memoryStream);
+                return memoryStream.ToArray();
+            }
+        }
+
+        public static Stream GetEmbeddedResourceStream(Assembly assembly, string resourceFileName)
+        {
+            var resourceNames = assembly.GetManifestResourceNames();
+
+            var resourcePaths = resourceNames
+                .Where(x => x.EndsWith(resourceFileName, StringComparison.CurrentCultureIgnoreCase))
+                .ToArray();
+
+            if (!resourcePaths.Any())
+            {
+                throw new Exception(string.Format("Resource ending with {0} not found.", resourceFileName));
+            }
+
+            if (resourcePaths.Count() > 1)
+            {
+                throw new Exception(string.Format("Multiple resources ending with {0} found: {1}{2}", resourceFileName, Environment.NewLine, string.Join(Environment.NewLine, resourcePaths)));
+            }
+
+            return assembly.GetManifestResourceStream(resourcePaths.Single());
+        }
+    }
+}
